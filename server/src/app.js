dc4c0cb1f115225f302dcd1c147e18545d05d324
vishu <vishnuparihar.delhi5@gmail.com>
2026-07-05 22:02:41 +0530

    Frontend Connected

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const multer = require("multer");
 const postModel = require("./models/post.models");
 const uploadFile = require("./services/storage.service");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -23,11 +30,11 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
 });
 
 app.get("/posts", async (req, res) => {
-    const post = await postModel.find();
+  const post = await postModel.find();
 
-    return res.status(200).json({
-        posts: post
-    })
-})
+  return res.status(200).json({
+    posts: post,
+  });
+});
 
 module.exports = app;
